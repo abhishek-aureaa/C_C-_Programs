6ff04e1c9bc07770f0ff51fbcd432d7caaaf65a9
abhishek-aureaa <aureallm2170@gmail.com>
2020-04-26 20:32:33 +0530

    ModulusSumProduct

---
+++ ModulusSumProduct.cpp
@@ -0,0 +1,43 @@
+// NewAssignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+int Moduls_Sum_Product(int arr[], int max_index, int modulas_val)
+{
+	int SUM = 0;
+	for (int i = 0; i <= max_index; i++)
+	{
+		arr[i] = (arr[i] % modulas_val);
+		SUM += arr[i];
+	}
+	//int SUM = arr[];
+	return (SUM%modulas_val);
+}
+
+int main()
+{
+  //  std::cout << "Hello World!\n";
+	//std::string temp =  buildPalindrome(std::string st)
+	//std::string temp = buildPalindrome("abcdc");
+	//std::string temp = buildPalindrome("abcdc");
+	/*
+	temp = buildPalindrome("abcded");
+	std::cout << temp<<std::endl;
+	temp = buildPalindrome("abc");
+	std::cout << temp <<std::endl;
+	*/
+	//std::string temp = buildPalindrome("abc");
+	//std::string temp = buildPalindrome("aba");
+	//std::cout << temp << std::endl;
+
+	int arr[] = { 12, 24, 8, 6 };
+	int ret = Moduls_Sum_Product(arr, 3, 12);
+	std::cout << ret;
+
+
+}
+
