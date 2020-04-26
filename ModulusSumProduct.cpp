07f0b07c45ace94b68dda6019e3ac815482dece1
abhishek-aureaa <aureallm2170@gmail.com>
2020-04-26 20:34:46 +0530

    Modified ModulusSumProduct.cpp

--- ModulusSumProduct.cpp
+++ ModulusSumProduct.cpp
@@ -14,26 +14,11 @@ int Moduls_Sum_Product(int arr[], int max_index, int modulas_val)
 		arr[i] = (arr[i] % modulas_val);
 		SUM += arr[i];
 	}
-	//int SUM = arr[];
 	return (SUM%modulas_val);
 }
 
 int main()
 {
-  //  std::cout << "Hello World!\n";
-	//std::string temp =  buildPalindrome(std::string st)
-	//std::string temp = buildPalindrome("abcdc");
-	//std::string temp = buildPalindrome("abcdc");
-	/*
-	temp = buildPalindrome("abcded");
-	std::cout << temp<<std::endl;
-	temp = buildPalindrome("abc");
-	std::cout << temp <<std::endl;
-	*/
-	//std::string temp = buildPalindrome("abc");
-	//std::string temp = buildPalindrome("aba");
-	//std::cout << temp << std::endl;
-
 	int arr[] = { 12, 24, 8, 6 };
 	int ret = Moduls_Sum_Product(arr, 3, 12);
 	std::cout << ret;
